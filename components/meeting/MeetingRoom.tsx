c77806e7c3e8360ad004cfa14dd4b23debad305e
prmdhaliwal <prmdhaliwal35@gmail.com>
2026-05-04 02:19:41 +1000

    Add custom livekit meeting shell

---
+++ components/meeting/MeetingRoom.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function MeetingRoom() {
+  return (
+    <div className="lk-video-conference">
+      <div className="lk-video-conference-inner" />
+    </div>
+  );
+}
