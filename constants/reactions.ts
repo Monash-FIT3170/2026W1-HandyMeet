0dbe4cc17738b8d3df8c5465fde8d746a0f8f57d
Richard Li <richard.li2004@hotmail.com>
2026-05-18 15:26:41 +1000

    86d27v3qb format code
    - run prettier to format created code

--- constants/reactions.ts
+++ constants/reactions.ts
@@ -1,11 +1,11 @@
-export const ReactionTopic = "reaction";
+export const ReactionTopic = 'reaction';
 
 export enum Reaction {
-  RaiseHand = "raiseHand",
-  ThumbsDown = "thumbsDown",
-  ThumbsUp = "thumbsUp",
-  Hello = "hello",
-  Tired = "tired",
-  Ok = "ok",
-  ThankYou = "thankYou",
+  RaiseHand = 'raiseHand',
+  ThumbsDown = 'thumbsDown',
+  ThumbsUp = 'thumbsUp',
+  Hello = 'hello',
+  Tired = 'tired',
+  Ok = 'ok',
+  ThankYou = 'thankYou',
 }
