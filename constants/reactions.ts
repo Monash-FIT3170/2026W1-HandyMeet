fbf4ab8b0b2e204bffdd114d1f1e1a6cee082913
Dasun-Udugoda <dasun.udugoda@gmail.com>
2026-05-27 13:54:41 +1000

    86d34baqt: add mapping between reaction gesture and caption text.
    - Adds improved visuals to the gesture captions

--- constants/reactions.ts
+++ constants/reactions.ts
@@ -9,3 +9,14 @@ export enum Reaction {
   Ok = 'ok',
   ThankYou = 'thankYou',
 }
+
+export const REACTION_MAP: Record<Reaction, { emoji: string; label: string }> =
+  {
+    [Reaction.RaiseHand]: { emoji: '✋', label: 'Raised their hand' },
+    [Reaction.ThumbsUp]: { emoji: '👍', label: 'Agrees' },
+    [Reaction.ThumbsDown]: { emoji: '👎', label: 'Disagrees' },
+    [Reaction.Hello]: { emoji: '👋', label: 'Says hello' },
+    [Reaction.Tired]: { emoji: '😴', label: 'Is tired' },
+    [Reaction.Ok]: { emoji: '👌', label: 'Is OK' },
+    [Reaction.ThankYou]: { emoji: '🙏', label: 'Says thank you' },
+  };
